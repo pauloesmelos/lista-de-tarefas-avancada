fadb000147896ba59c6cf54a22ea62db9b92f190
pauloesmelos <email>
2023-01-20 00:25:36 -0300

    added featura: modal and responsiveness

--- script/js/main.js
+++ script/js/main.js
@@ -4,10 +4,13 @@ export default function initMain(){
     const button = document.querySelector('.add-tarefa');
     const divTable = document.querySelector('.div-table');
     const darkerModal = document.querySelector('.darker');
+    const formEdit = document.forms.formEditar;//edit
+    const buttonAdd = document.querySelector('.button-edit');//edit
+    //const tbody = document.querySelector('.tabela-corpo');
 
     //objeto com os dados de todos inputs
     const inputs = {//pegando dados do form de forma dinamica
-        descricao: {//se setarmos
+        descricao: {
             tarefa: '',
             prazo: '',
             tipo: 'Rotina',
@@ -26,10 +29,32 @@ export default function initMain(){
             this.descricao.status = valor;
         }
     }
-
+    const edit = {//objeto com os valores que o usuário ira editar
+        tr: null,
+        tarefas(valor){
+            this.valueTarefa = valor;
+        },
+        prazo(valor){
+            this.valuePrazo = valor;
+        },
+        tipo(valor){
+            this.valueTipo = valor;
+        },
+        status(valor){
+            this.valueStatus = valor;
+        }
+    };
     //funcoes
+    const messagemError = (elemento,bool) => {
+        const span = document.querySelector(elemento);
+        if(bool)
+            span.classList.add('ativo');
+        else
+            span.classList.remove('ativo');
+    };
     const getTarefa = (event) => {
         inputs[event.target.id](event.target.value);//pegando dados do form de forma dinamica
+        messagemError('.error',false);//passar o span com a mensagem de erro
     };
     const isEmpty = (elemento1,elemento2) => {
         return !elemento1 || !elemento2;
@@ -80,7 +105,7 @@ export default function initMain(){
             button.classList.add('adicionada');
             const timer = setTimeout(() => {
                 button.classList.remove('adicionada');
-            },2000);
+            },1000);
         },0);
     };
     const setTarefa = (event) => {//cria a tarefa e add na tabela
@@ -96,11 +121,29 @@ export default function initMain(){
                 createTask('.tabela-corpo');
                 animaButton();
             }
-                
+            else if(isEmpty(inputs.descricao.tarefa,inputs.descricao.prazo))
+                messagemError('.error',true);
         }
         catch(erro){
             console.log('erro tipo ', erro);
         }
+    };
+    const trQueSeraMudado = (tr) => {
+        edit.tr = tr;
+    };
+    const preencherInputsEditar = () => {
+        const tr = [...edit.tr.children];
+        const filhos = [...formEdit.children];
+        const inputs = filhos.map((e) => {
+            let input = null;
+            if(e.tagName === 'INPUT' || e.tagName === 'SELECT')
+                input = e;
+            return input;
+        }).filter(e => e !== null);//pegando os inputs do formulario de edição e elimando os labels
+
+        inputs.forEach((e,i) => {
+            e.value = tr[i].innerText;      
+        });
     };
     const alterarTarefa = (event) => {//editar ou excluir
         const tr = event.target.closest('tr');//função que pega o primeiro elemento informado no parametro
@@ -108,11 +151,45 @@ export default function initMain(){
         if(event.target.classList.contains('editar')){
             darkerModal.classList.add('ativo');
             darkerModal.nextElementSibling.classList.add('ativo');//nextElementSibling ou selecionar o modal com document.querySelector,tanto faz
+            trQueSeraMudado(tr);
+            preencherInputsEditar();//preenche com os dados originais
         }
     };
-    
+    const getTexto = (event) => {
+        edit[event.target.name](event.target.value)//[] é uma altenativa ao . ex: edit.tarefas === edit[tarefas]
+    };
+    const editarTr = (array) => {
+        const tr = [...edit.tr.children];//tr original - capturado na função alterarTarefa com a função closest() - que retorna o ancestral mais proximo - elemento pai 
+        tr.forEach((e,i) => {
+            if(e.classList.contains('stt') && e.innerText !== array[i]){//verifica se mudou o tipo do status - realizado ou pendente
+                const status = array[i];
+                switch(status){
+                    case 'Pendente':
+                        e.setAttribute('class','stt pendente');
+                        console.log(e);
+                        break;
+                    case 'Realizado':
+                        e.setAttribute('class','stt realizado');
+                        console.log(e);
+                        break;
+                }
+            }
+            e.innerText !== '' ? e.innerText = array[i] : undefined;
+        });
+    }
+    const editarTarefa = () => {
+        //criar um array de tarefas pra editar na tabela de forma dinamica - num forEach
+        const array = [];
+        array.push(edit.valueTarefa = (edit.valueTarefa === undefined) ? inputs.descricao.tarefa : edit.valueTarefa);
+        array.push(edit.valuePrazo = (edit.valuePrazo === undefined) ? inputs.descricao.prazo : edit.valuePrazo);
+        array.push(edit.valueTipo = (edit.valueTipo === undefined) ? 'Rotina' : edit.valueTipo);
+        array.push(edit.valueStatus = (edit.valueStatus === undefined) ? 'Pendente' : edit.valueStatus);
+        editarTr(array);
+    }
     //add os eventos
     form.addEventListener('input',getTarefa);
     button.addEventListener('click',setTarefa);
     divTable.addEventListener('click',alterarTarefa);
+    formEdit.addEventListener('input',getTexto);
+    buttonAdd.addEventListener('click',editarTarefa);
 }
--- script/script.js
+++ script/script.js
@@ -4,11 +4,9 @@ import initDarkMode from './js/dark-mode.js';
 import initData from './js/data.js';
 import initMain from './js/main.js';
 import initCloseModal from './js/close-modal.js';
-import initEditarTask from './js/editar-task.js';
 //chamada
 initMenuLateral();
 initDarkMode();
 initData();
 initMain();
 initCloseModal();
-initEditarTask();
